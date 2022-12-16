d433017179793ea553311cd840457a420e37aeb2
Satar Emami <satar@artemis.com>
2022-12-16 19:22:51 +0330

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default function App(props: any) {
+    return (
+        <>
+            <div style={{backgroundColor:"bisque",height:"100%",width:"100%",display:"flex",justifyContent:"center",alignItems:"center"}}>
+                salam
+            </div>
+        </>
+    );
+}
